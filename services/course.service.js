167a8988fd201806075eb502bf696a196a32fe9b
Sergey Batukov <sashachepuha@gmail.com>
2022-01-19 19:09:23 +0300

    add chart

--- services/course.service.js
+++ services/course.service.js
@@ -18,23 +18,37 @@ const getCourse = async function (pair, limit) {
     .then(function (response) {
       console.log("\x1Bc");
       const data = response.data;
-      Object.keys(data).forEach((key) => {
-        cnsl.course({
-          pair: key,
-          up: true,
-          price: data[key][0].price,
-          volumes: data[key].map((itm) => {
-            return {
-              time: cnsl.dateParse(itm.date),
-              val: itm.price,
-            };
-          }),
-        });
+      let sort = data[pair]
+        .map((itm) => {
+          return +itm.price;
+        })
+        .sort((a, b) => a - b);
+      const min = Math.floor(sort[0]);
+      const max = Math.floor(sort[data[pair].length - 1]);
+      cnsl.course({
+        pair,
+        up: true,
+        price: data[pair][0].price,
+        volumes: data[pair].map((itm) => {
+          return {
+            time: cnsl.dateParse(itm.date),
+            val:
+              parseFloat(itm.price).toFixed(2) +
+              chartLine(+itm.price, min, max),
+          };
+        }),
       });
     })
     .catch(function (error) {
       console.log(error);
     });
+};
+
+const chartLine = (val, min, max) => {
+  const step = (max - min) / 100;
+  const size = 50;
+  const percent = (val - min) / step;
+  return "    " + chalk.grey("▬").repeat(Math.floor(size * (percent / 100)));
 };
 
 export { getCourse };
